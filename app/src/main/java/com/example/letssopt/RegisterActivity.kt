24c9afdf6c5a462c0995d5877987787f249a895f
dodo <eg4rang@naver.com>
2026-04-06 03:00:43 +0900

    [Feat] 회원가입 화면 구현

---
+++ app/src/main/java/com/example/letssopt/RegisterActivity.kt
@@ -0,0 +1,150 @@
+package com.example.letssopt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.letssopt.component.ButtonPrimary
+import com.example.letssopt.component.LogoText
+import com.example.letssopt.component.TextFieldDefault
+import com.example.letssopt.ui.theme.LETSSOPTTheme
+
+class RegisterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val emailState = rememberTextFieldState()
+            val passwordState = rememberTextFieldState()
+            val passwordCheckState = rememberTextFieldState()
+
+            val registerBtnEnabled = emailState.text.isNotBlank()
+                    && passwordState.text.isNotBlank()
+                    && passwordCheckState.text.isNotBlank()
+
+            LETSSOPTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RegisterScreen(
+                        emailState = emailState,
+                        passwordState = passwordState,
+                        passwordCheckState = passwordCheckState,
+                        onRegisterBtnClick = {},
+                        registerBtnEnabled = registerBtnEnabled,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RegisterScreen(
+    emailState: TextFieldState,
+    passwordState: TextFieldState,
+    passwordCheckState: TextFieldState,
+    onRegisterBtnClick: () -> Unit,
+    registerBtnEnabled: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        LogoText(
+            modifier = Modifier.padding(top = 60.dp, bottom = 26.dp)
+        )
+
+        Text(
+            text = "회원가입",
+            modifier = Modifier.align(Alignment.Start),
+            color = LETSSOPTTheme.colors.textPrimary,
+            style = LETSSOPTTheme.typography.h2,
+        )
+
+        Spacer(Modifier.height(36.dp))
+
+        TextFieldDefault(
+            state = emailState,
+            placeholder = "이메일 주소를 입력하세요",
+            label = "이메일",
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            onKeyboardAction = {
+                focusManager.moveFocus(FocusDirection.Next)
+            },
+        )
+
+        Spacer(Modifier.height(18.dp))
+
+        TextFieldDefault(
+            state = passwordState,
+            placeholder = "비밀번호를 입력하세요",
+            label = "비밀번호",
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            onKeyboardAction = {
+                focusManager.moveFocus(FocusDirection.Next)
+            },
+        )
+
+        Spacer(Modifier.height(18.dp))
+
+        TextFieldDefault(
+            state = passwordCheckState,
+            placeholder = "비밀번호를 다시 입력하세요",
+            label = "비밀번호 확인",
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            onKeyboardAction = {
+                keyboardController?.hide()
+                focusManager.clearFocus()
+            },
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        ButtonPrimary(
+            text = "회원가입",
+            onClick = onRegisterBtnClick,
+            enabled = registerBtnEnabled,
+        )
+
+        Spacer(Modifier.height(26.dp))
+    }
+}
+
+@Preview
+@Composable
+private fun RegisterScreenPreview() {
+    LETSSOPTTheme {
+        RegisterScreen(
+            emailState = rememberTextFieldState(),
+            passwordState = rememberTextFieldState(),
+            passwordCheckState = rememberTextFieldState(),
+            onRegisterBtnClick = {},
+            registerBtnEnabled = true,
+        )
+    }
+}
