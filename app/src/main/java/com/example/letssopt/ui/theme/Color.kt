1346c2a1ebd138cd262b4c82fdd2bb7017509049
dodo <eg4rang@naver.com>
2026-04-05 20:20:33 +0900

    [Feat] 컬러 시스템 설정

--- app/src/main/java/com/example/letssopt/ui/theme/Color.kt
+++ app/src/main/java/com/example/letssopt/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.letssopt.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryRed = Color(0xFFE8003C)
+val Background = Color(0xFF000000)
+val Surface = Color(0xFF2A2A2A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFF999999)
+val Placeholder = Color(0xFF666666)
