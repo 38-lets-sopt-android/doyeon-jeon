64fc722fe52cb46335dd67dda0bd85d6758a76cc
dodo <eg4rang@naver.com>
2026-04-19 17:11:08 +0900

    [Chore] 디자인시스템 Black 컬러 추가

--- app/src/main/java/com/example/letssopt/core/designsystem/theme/Color.kt
+++ app/src/main/java/com/example/letssopt/core/designsystem/theme/Color.kt
@@ -5,12 +5,14 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 val PrimaryRed = Color(0xFFE8003C)
-val Background = Color(0xFF000000)
+val Background = Color(0xFF141414)
+
 val Surface = Color(0xFF2A2A2A)
 val White = Color(0xFFFFFFFF)
 val TextSecondary = Color(0xFF999999)
 val Placeholder = Color(0xFF666666)
 val Disabled = Color(0xFF333333)
+val Black = Color(0xFF000000)
 
 @Immutable
 class LETSSOPTColors(
@@ -21,6 +23,7 @@ class LETSSOPTColors(
     val textSecondary: Color,
     val placeHolder: Color,
     val disabled: Color,
+    val black: Color,
 )
 
 val defaultLETSSOPTColors = LETSSOPTColors(
@@ -31,6 +34,7 @@ val defaultLETSSOPTColors = LETSSOPTColors(
     textSecondary = TextSecondary,
     placeHolder = Placeholder,
     disabled = Disabled,
+    black = Black,
 )
 
 val LocalLETSSOPTColorsProvider = staticCompositionLocalOf { defaultLETSSOPTColors }
