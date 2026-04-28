c9c6317e4ef059c91980d508b88453b48dd9f3d1
dodo <eg4rang@naver.com>
2026-04-28 23:28:10 +0900

    [Fix] DatabaseModule의 init이 getDatabase 사용하도록 변경

--- app/src/main/java/com/example/letssopt/data/di/DatabaseModule.kt
+++ app/src/main/java/com/example/letssopt/data/di/DatabaseModule.kt
@@ -1,17 +1,13 @@
 package com.example.letssopt.data.di
 
 import android.content.Context
-import androidx.room.Room
 import com.example.letssopt.data.local.AppDatabase
 
 object DatabaseModule {
     private lateinit var database: AppDatabase
 
-    fun init (context: Context) {
-        database = Room.databaseBuilder(
-            context,
-            AppDatabase::class.java, "database-name"
-        ).build()
+    fun init(context: Context) {
+        database = AppDatabase.getDatabase(context)
     }
 
     val favoriteDao by lazy { database.favoriteDao() }
