0e6cc09b70c56503c1ce1abe04121cd54bf3caa4
dodo <eg4rang@naver.com>
2026-04-28 18:42:57 +0900

    [Feat] MainNavHost 구현

---
+++ app/src/main/java/com/example/letssopt/presentation/main/navigation/MainNavHost.kt
@@ -0,0 +1,48 @@
+package com.example.letssopt.presentation.main.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.example.letssopt.core.navigation.Route
+import com.example.letssopt.presentation.archive.navigation.archiveNavGraph
+import com.example.letssopt.presentation.auth.navigation.authNavGraph
+import com.example.letssopt.presentation.home.navigation.homeNavGraph
+import com.example.letssopt.presentation.purchase.navigation.purchaseNavGraph
+import com.example.letssopt.presentation.search.navigation.searchNavGraph
+import com.example.letssopt.presentation.webtoon.navigation.webtoonNavGraph
+
+@Composable
+fun MainNavHost(
+    navigator: MainNavigator,
+    paddingValues: PaddingValues,
+    startDestination: Route,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navigator.navController,
+        startDestination = startDestination,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        authNavGraph(
+            navController = navigator.navController,
+            paddingValues = paddingValues,
+        )
+        homeNavGraph(
+            paddingValues = paddingValues,
+        )
+        purchaseNavGraph(
+            paddingValues = paddingValues,
+        )
+        webtoonNavGraph(
+            paddingValues = paddingValues,
+        )
+        searchNavGraph(
+            paddingValues = paddingValues,
+        )
+        archiveNavGraph(
+            paddingValues = paddingValues,
+        )
+    }
+}
