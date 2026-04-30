f7dd46c10c7b2887714f6d6800e788eb44656b14
dodo <eg4rang@naver.com>
2026-04-30 23:33:29 +0900

    [Refactor] isLoggedIn 결과가 null인 경우 시작 destination Auth로 설정

--- app/src/main/java/com/example/letssopt/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/letssopt/presentation/main/MainViewModel.kt
@@ -2,7 +2,7 @@ package com.example.letssopt.presentation.main
 
 import androidx.lifecycle.ViewModel
 import com.example.letssopt.core.navigation.Route
-import com.example.letssopt.data.auth.AuthRepository
+import com.example.letssopt.data.di.RepositoryModule
 import com.example.letssopt.presentation.auth.navigation.AuthGraph
 import com.example.letssopt.presentation.home.navigation.HomeRoute
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,13 +10,14 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class MainViewModel : ViewModel() {
+    private val authRepository = RepositoryModule.authRepository
     private val _startDestination = MutableStateFlow<Route?>(null)
     val startDestination = _startDestination.asStateFlow()
 
     init {
-        val dest = when (AuthRepository.isLoggedIn()) {
+        val dest = when (authRepository.getIsLoggedIn().getOrNull()) {
             true -> HomeRoute.Home
-            false -> AuthGraph
+            else -> AuthGraph
         }
         _startDestination.update { dest }
     }
